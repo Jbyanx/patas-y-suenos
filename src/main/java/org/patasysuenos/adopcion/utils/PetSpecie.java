4b89740822e1476be71ab8f1163cd055ec8d4bc9
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-28 13:33:31 -0500

    agregando la enumeracion PetSpecie para los tipos de animales

---
+++ src/main/java/org/patasysuenos/adopcion/utils/PetSpecie.java
@@ -0,0 +1,11 @@
+package org.patasysuenos.adopcion.utils;
+
+public enum PetSpecie {
+    DOG,        // Perro
+    CAT,        // Gato
+    BIRD,       // Ave
+    RABBIT,     // Conejo
+    HAMSTER,    // Hámster
+    TURTLE,     // Tortuga
+    FISH       // Pez
+}
