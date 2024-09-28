89601521b4b54c3ca0a0035c74dea5f921653ee5
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-28 13:11:10 -0500

    agregando libreria mapstruct

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
+	id ("net.ltgt.apt") version "0.21"
 }
 
 group = "org.patasysuenos"
@@ -26,9 +27,11 @@ repositories {
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation ("org.mapstruct:mapstruct:1.6.2")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
+	annotationProcessor ("org.mapstruct:mapstruct-processor:1.6.2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
