4f314e7cdb516265d76f5e37d22923b3fbb485cd
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-28 14:21:20 -0500

    agregando el starter de validations

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation ("org.mapstruct:mapstruct:1.6.2")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
